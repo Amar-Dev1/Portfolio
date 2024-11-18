02c71dc172bf82d4b2d2787048ddcf6e6c0e8425
Amar Yasir <3marofficial249@gmail.com>
2024-11-18 16:14:14 +0200

    last check , for real

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -4,26 +4,33 @@ import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa6';
 import CardsData from '../../Components/ProjectsCard/CardsData';
 import IntroProjectsCard from '../../Components/ProjectsCard/IntroProjectsCard';
+import { useEffect } from 'react';
 const Projects = () => {
+
+  // let the screen scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
   return (
     <Container fluid id='projects'>
       <button className='amar-btn back-btn'><Link to="/"><FaArrowLeft /> Home</Link></button>
       <Container className='projects-wrapper'>
         <h2 className='title'>Awesome Projects I`have Built 🤓</h2>
         <Row className='mt-5 mb-5'>
-        {
-          CardsData.map((card, index) => {
-            return (
-              <Col key={index} className='d-flex justify-content-center mt-3 card-col' xxl={3} lg={4} md={6}>
-                <IntroProjectsCard
-                  title={card.title}
-                  des={card.des}
-                  img={card.img}
-                />
-              </Col>
-            )
-          })
-        }
+          {
+            CardsData.map((card, index) => {
+              return (
+                <Col key={index} className='d-flex justify-content-center mt-3 card-col' xxl={3} lg={4} md={6}>
+                  <IntroProjectsCard
+                    title={card.title}
+                    des={card.des}
+                    img={card.img}
+                  />
+                </Col>
+              )
+            })
+          }
         </Row>
       </Container>
     </Container>
