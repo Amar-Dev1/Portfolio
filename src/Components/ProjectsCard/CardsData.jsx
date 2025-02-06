3013991e0d13dce7148cef64861a8a51fb645d27
Amar Yasir <3marofficial249@gmail.com>
2025-02-06 20:29:38 +0200

    some changes

--- src/Components/ProjectsCard/CardsData.jsx
+++ src/Components/ProjectsCard/CardsData.jsx
@@ -9,7 +9,7 @@ const CardsData = [
     { id: 2, title: "DashboardUI", des: "a responsive dashboard showing the risks of Cybernetics Risks and detect the Surfaces that are vulnerable to penetration. made with React.js, Bootstrap and graph.js liberary", img: dashboardImg, live: "https://challenge-five-alpha.vercel.app", code: "https://github.com/Amar-Dev1/Challenge" },
     { id: 3, title: "Phenix Gym", des: "it`s a modren responsive website for Gym guiding and coaching for changing bodies and lives. made with React.js.", img: phenixImg, live: "https://phenix-gym.vercel.app/", code: "https://github.com/Amar-Dev1/Phenix-Gym" },
     { id: 4, title: "SanTech", des: "a simple responsive ecommerce website for selling staff like : phones, headsets, accessories and watches. made with React.js.", img: santechImg, live: "https://san-tech-sigma.vercel.app/", code: "https://github.com/Amar-Dev1/San-Tech" },
-    { id: 5, title: "A-Calculator", des: "It's a Nodejs API allows you to do CRUD operations with flashcards including games to help you memorize better with implemented Spaced Repetition methodology.", img: calcImg, live: "https://amar-dev1.github.io/A-Calculator", code: "https://github.com/Amar-Dev1/A-Calculator" },
+    { id: 5, title: "A-Calculator", des: "It's a Simple calculator with screen switch mode. made for fun 🤝", img: calcImg, live: "https://amar-dev1.github.io/A-Calculator", code: "https://github.com/Amar-Dev1/A-Calculator" },
 
 
 ];
