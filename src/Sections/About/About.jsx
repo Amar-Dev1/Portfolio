ed70589165b02b67429f60da0daea781db19a026
Amar Yasir <3marofficial249@gmail.com>
2025-02-09 21:59:26 +0200

    some adjustments1

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -11,9 +11,9 @@ const About = () => {
             <h2 className="title">About Me</h2>
             <div className='about-phrase'>
               <p>
-                My name is <span className="my-name">Amar</span>. Passionate computer science student with a focus on web development. Experienced with technologies like Python,
-Django, React.js, and Flask, and always eager to learn and contribute to impactful projects. Looking for opportunities to
-grow as a software engineer and work on projects that improve lives.</p>
+                My name is <span className="my-name">Amar</span>. Passionate computer science student with a focus on web development. Experienced with: <span className='text-dark'>Python,
+                Django, React.js, and Flask.</span> Always eager to learn and contribute to impactful projects. Looking for opportunities to
+                grow as a software engineer and work on projects that improve lives.</p>
             </div>
           </Col>
           <Col lg={6} md={12} sm={12} className="d-flex justify-content-center align-items-center">
