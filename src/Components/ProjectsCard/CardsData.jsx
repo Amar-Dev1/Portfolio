43652a55584e7aeeac8937c81a31a3f80ee57e95
Amar <3marofficial249@gmail.com>
2025-03-31 00:28:12 +0200

    added GGlife

--- src/Components/ProjectsCard/CardsData.jsx
+++ src/Components/ProjectsCard/CardsData.jsx
@@ -1,18 +1,58 @@
-import l4sImg from '../../assets/l4s-img.png';
-import phenixImg from '../../assets/phenix-img.png';
-import dashboardImg from '../../assets/dashboard-img.png';
-import santechImg from '../../assets/santech-img.png';
-import calcImg from '../../assets/a-calculator.png';
-import lemonImg from '../../assets/lemonproject.png';
+import l4sImg from "../../assets/l4s-img.png";
+import phenixImg from "../../assets/phenix-img.png";
+import dashboardImg from "../../assets/dashboard-img.png";
+import santechImg from "../../assets/santech-img.png";
+import lemonImg from "../../assets/lemonproject.png";
+import gglifeImg from "../../assets/gglife-img.png";
 const CardsData = [
-    { id: 1, title: "Littlelemon", des: "full-stack web application for LittleLemon restaurant, with a lot of features. This project made with React.js for UI ⚡ and Django for backend ✨",img:lemonImg,live:'https://little-lemon-p576.vercel.app',code :"https://github.com/Amar-Dev1/LittleLemon" },
-    { id: 2, title: "DashboardUI", des: "a responsive dashboard showing the risks of Cybernetics Risks and detect the Surfaces that are vulnerable to penetration. made with React.js, Bootstrap and graph.js liberary", img: dashboardImg, live: "https://challenge-five-alpha.vercel.app", code: "https://github.com/Amar-Dev1/Challenge" },
-    { id: 3, title: "L4S", des: "L4S is a website for learn4sudan non-profit organization consist of a lot of cool stuff✨. made with React.js, bootstrap and Sanity for content management.", img: l4sImg, live: "https://learn4-sudan-a8rv.vercel.app/", code: "https://github.com/Amar-Dev1/Learn4Sudan" },
-    { id: 4, title: "Phenix Gym", des: "it`s a modren responsive website for Gym guiding and coaching for changing bodies and lives. made with React.js.", img: phenixImg, live: "https://phenix-gym.vercel.app/", code: "https://github.com/Amar-Dev1/Phenix-Gym" },
-    { id: 5, title: "SanTech", des: "a simple responsive ecommerce website for selling staff like : phones, headsets, accessories and watches. made with React.js.", img: santechImg, live: "https://san-tech-sigma.vercel.app/", code: "https://github.com/Amar-Dev1/San-Tech" },
-    { id: 6, title: "A-Calculator", des: "It's a Simple calculator with screen switch mode. made for fun 🤝", img: calcImg, live: "https://amar-dev1.github.io/A-Calculator", code: "https://github.com/Amar-Dev1/A-Calculator" },
-
-
+  {
+    id: 1,
+    title: "Littlelemon",
+    des: "full-stack web application for LittleLemon restaurant, with a lot of features. This project made with React.js for UI ⚡ and Django for backend ✨",
+    img: lemonImg,
+    live: "https://little-lemon-p576.vercel.app",
+    code: "https://github.com/Amar-Dev1/LittleLemon",
+  },
+  {
+    id: 2,
+    title: "GGLife",
+    des: "GGLife is a full-stack web app 🚀 designed for structured weekly planning, enabling users to set clear goals, create action plans, and track progress effortlessly. The modern UI is built with React & TypeScript ⚡, while the backend is currently in development with Express. ✨",
+    img: gglifeImg,
+    live: "https://gglife.vercel.app",
+    code: "https://github.com/Amar-Dev1/GGLifeUI",
+  },
+  {
+    id: 3,
+    title: "DashboardUI",
+    des: "a responsive dashboard showing the risks of Cybernetics Risks and detect the Surfaces that are vulnerable to penetration. made with React.js, Bootstrap and graph.js liberary",
+    img: dashboardImg,
+    live: "https://challenge-five-alpha.vercel.app",
+    code: "https://github.com/Amar-Dev1/Challenge",
+  },
+  {
+    id: 4,
+    title: "L4S",
+    des: "L4S is a website for learn4sudan non-profit organization consist of a lot of cool stuff✨. made with React.js, bootstrap and Sanity for content management.",
+    img: l4sImg,
+    live: "https://learn4-sudan-a8rv.vercel.app/",
+    code: "https://github.com/Amar-Dev1/Learn4Sudan",
+  },
+  {
+    id: 5,
+    title: "Phenix Gym",
+    des: "it`s a modren responsive website for Gym guiding and coaching for changing bodies and lives. made with React.js.",
+    img: phenixImg,
+    live: "https://phenix-gym.vercel.app/",
+    code: "https://github.com/Amar-Dev1/Phenix-Gym",
+  },
+  {
+    id: 6,
+    title: "SanTech",
+    des: "a simple responsive ecommerce website for selling staff like : phones, headsets, accessories and watches. made with React.js.",
+    img: santechImg,
+    live: "https://san-tech-sigma.vercel.app/",
+    code: "https://github.com/Amar-Dev1/San-Tech",
+  },
 ];
 
 export default CardsData;
